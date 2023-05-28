3733a3499092113c6699e7f8ef45dfe9e23ff410
Owen Sun <114966159+osun24@users.noreply.github.com>
2023-05-27 21:03:42 -0700

    Add comments to TimerStopwatchScript.cs

--- TimerStopwatchScript.cs
+++ TimerStopwatchScript.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 
+// Uses TextMeshPro Text
 public class TimerStopwatchScript : MonoBehaviour
 {
     [Header("Timer Settings")]
@@ -15,9 +16,12 @@ public class TimerStopwatchScript : MonoBehaviour
 
     private void Update()
     {
+        // Check if it's a countdown - if it is, decrease currentTime by the time that has passed. If it's not a countdown, add the time that has passed.
         currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
-
+        
+        // Check if there's a limit on the clock, if there is check and see if that limit has been reached
         if (hasLimit && ((countDown && currentTime <= timerLimit) || (!countDown && currentTime >= timerLimit))){
+            // Lock the time at the limit and stop updating the time
             currentTime = timerLimit;
             SetTimerText();
             timerText.color = Color.red;
@@ -29,8 +33,13 @@ public class TimerStopwatchScript : MonoBehaviour
 
     private void SetTimerText()
     {
+        // Convert currentTime (seconds) to minutes by flooring (gets the integer quotient of currentTime divided by 60)
         double minutes = Math.Floor(currentTime/60);
+        
+        // Get the modulus (remainder) of the currentTime divided by 60
         double seconds = currentTime % 60;
+        
+        // Convert the minutes & seconds to strings with a format of MM:SS
         string time = $"{minutes.ToString("00")}:{seconds.ToString("00")}";
 
         timerText.text = time;
